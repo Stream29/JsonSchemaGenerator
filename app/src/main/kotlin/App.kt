63bfb5f5a6c2a269099fe7f75df2ef87fb63a64d
Stream <1542763342@qq.com>
2024-12-13 18:41:28 +0800

    feat: init by IDEA

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,11 @@
+package io.github.stream29.app
+
+import io.github.stream29.utils.Printer
+
+// This is the main entry point of the application.
+// It uses the `Printer` class from the `:utils` subproject.
+fun main() {
+    val message = "Hello JetBrains!"
+    val printer = Printer(message)
+    printer.printMessage()
+}
