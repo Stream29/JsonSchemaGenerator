ca05bd2ab09f6b9f617babe374fee559b56ca1c1
Stream <1542763342@qq.com>
2025-01-07 12:33:06 +0800

    docs: add KDoc for SchemaGenerator.kt

--- json-schema-generator/src/commonMain/kotlin/io/github/stream29/jsonschemagenerator/SchemaGenerator.kt
+++ json-schema-generator/src/commonMain/kotlin/io/github/stream29/jsonschemagenerator/SchemaGenerator.kt
@@ -7,8 +7,25 @@ import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.serializer
 
+/**
+ * Function type of instructions to generate a schema for certain type.
+ */
 public typealias SchemaGeneratingInstruction = SchemaBuildingContext.() -> JsonObject
 
+/**
+ * A generator to generate a JSON schema for a given type.
+ *
+ * It is a immutable class so it can be shared across multiple threads.
+ *
+ * @property encodePrimitive Instruction to generate schema for primitive types.
+ * @property encodeEnum Instruction to generate schema for enum types.
+ * @property encodeClass Instruction to generate schema for class types.
+ * @property encodeArray Instruction to generate schema for array types.
+ * @property encodeMap Instruction to generate schema for map types.
+ * @property encodePolymorphic Instruction to generate schema for polymorphic types.
+ * @property encodeContextual Instruction to generate schema for contextual types.
+ * @property encodeObject Instruction to generate schema for singleton object types.
+ */
 @OptIn(ExperimentalSerializationApi::class)
 public data class SchemaGenerator(
     val encodePrimitive: SchemaGeneratingInstruction = {
@@ -81,16 +98,28 @@ public data class SchemaGenerator(
     val encodeContextual: SchemaGeneratingInstruction = { TODO() },
     val encodeObject: SchemaGeneratingInstruction = { TODO() },
 ) {
+    /**
+     * Global default for [SchemaGenerator].
+     */
     public companion object Default {
         public val default: SchemaGenerator = SchemaGenerator()
     }
 }
 
+/**
+ * Generate a schema for the given type [T].
+ */
 public inline fun <reified T> SchemaGenerator.Default.from(): JsonObject = default.schemaOf<T>()
 
+/**
+ * Generate a schema for the given type [T].
+ */
 public inline fun <reified T> SchemaGenerator.schemaOf(): JsonObject =
     schemaOf(serializer<T>().descriptor, emptyList())
 
+/**
+ * Generate a schema for the given [descriptor] with [annotations].
+ */
 @OptIn(ExperimentalSerializationApi::class)
 public fun SchemaGenerator.schemaOf(descriptor: SerialDescriptor, annotations: List<Annotation>): JsonObject {
     val context = SchemaBuildingContext(descriptor, annotations + descriptor.annotations, this)
